2800e6c807b683b8e111536d8592331b86e9ec51
Johannes Rudolph <johannes_rudolph@gmx.de>
2012-02-03 11:59:42 +0100

    moved TemplateMagic into the API

--- templates/src/main/scala/ScalaTemplates.scala
+++ templates/src/main/scala/ScalaTemplates.scala
@@ -472,7 +472,7 @@ package play.templates {
 package """ :+ packageName :+ """
 
 import play.templates._
-import play.templates.TemplateMagic._
+import play.api.templates.TemplateMagic._
 
 """ :+ additionalImports :+ """
 /*""" :+ root.comment.map(_.msg).getOrElse("") :+ """*/
@@ -711,74 +711,4 @@ object """ :+ name :+ """ extends BaseScalaTemplate[""" :+ resultType :+ """,For
     }
 
   }
-
-  /* ------ */
-
-  object TemplateMagic {
-
-    // --- UTILS
-
-    def defining[T](t: T)(handler: T => Any) = {
-      handler(t)
-    }
-
-    def using[T](t: T) = t
-
-    // --- IF
-
-    implicit def iterableToBoolean(x: Iterable[_]) = x != null && !x.isEmpty
-    implicit def optionToBoolean(x: Option[_]) = x != null && x.isDefined
-    implicit def stringToBoolean(x: String) = x != null && !x.isEmpty
-
-    // --- JAVA
-
-    implicit def javaCollectionToScala[T](x: java.lang.Iterable[T]) = {
-      import scala.collection.JavaConverters._
-      x.asScala
-    }
-
-    // --- DEFAULT
-
-    case class Default(default: Any) {
-      def ?:(x: Any) = x match {
-        case "" => default
-        case Nil => default
-        case false => default
-        case 0 => default
-        case None => default
-        case _ => x
-      }
-    }
-
-    implicit def anyToDefault(x: Any) = Default(x)
-
-    // --- DATE
-
-    class RichDate(date: java.util.Date) {
-
-      def format(pattern: String) = {
-        new java.text.SimpleDateFormat(pattern).format(date)
-      }
-
-    }
-
-    implicit def richDate(date: java.util.Date) = new RichDate(date)
-
-    // --- STRING
-
-    class RichString(string: String) {
-
-      def when(predicate: => Boolean) = {
-        predicate match {
-          case true => string
-          case false => ""
-        }
-      }
-
-    }
-
-    implicit def richString(string: String) = new RichString(string)
-
-  }
-
 }
