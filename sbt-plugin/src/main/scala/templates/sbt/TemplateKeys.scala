0df56dfbf9110ef76932b20c1f4efdff19c6ce2b
Johannes Rudolph <johannes_rudolph@gmx.de>
2012-02-05 19:24:13 +0100

    cleanup key names

--- sbt-plugin/src/main/scala/templates/sbt/TemplateKeys.scala
+++ sbt-plugin/src/main/scala/templates/sbt/TemplateKeys.scala
@@ -19,8 +19,8 @@ import java.io.File
 import sbt._
 
 trait TemplateKeys {
-  val templatesImport = SettingKey[Seq[String]]("play-templates-imports", "Additional imports for the templates")
-  val templatesTypes = SettingKey[PartialFunction[String, (String, String)]]("play-templates-formats", "Defined template formats")
+  val templatesImport = SettingKey[Seq[String]]("templates-imports", "Additional imports for the templates")
+  val templatesTypes = SettingKey[PartialFunction[String, (String, String)]]("templates-formats", "Defined template formats")
 
   val compileTemplates = TaskKey[Seq[File]]("compile-templates", "Compile scala templates into scala files")
   val templatesReportErrors = TaskKey[inc.Analysis]("templates-report-errorrs", "Reports template errors")
