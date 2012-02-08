2d09a8f9bcdf2aa8a11a981fefdaa64b37cfdde3
Mathias <mathias@decodified.com>
2012-02-08 17:24:27 +0100

    Clean up and extend build definition

---
+++ project/Build.scala
@@ -0,0 +1,72 @@
+import sbt._
+import Keys._
+
+object Build extends Build {
+  import Dependencies._
+
+  // configure prompt to show current project
+  override lazy val settings = super.settings :+ {
+    shellPrompt := { s => Project.extract(s).currentProject.id + " > " }
+  }
+
+  lazy val root =
+    Project("twirl", file("."))
+      .aggregate(twirlApi, twirl, sbtPlugin)
+
+  lazy val twirlApi =
+    Project("twirl-api", file("twirl-api"))
+      .settings(generalSettings: _*)
+      .settings(
+        libraryDependencies += commonsLang
+      )
+
+  lazy val twirl =
+    Project("twirl-compiler", file("twirl-compiler"))
+      .settings(generalSettings: _*)
+      .settings(
+        libraryDependencies ++= Seq(
+          scalaIO,
+          Test.specs
+        ),
+        libraryDependencies <+= scalaVersion(scalaCompiler)
+      )
+      .dependsOn(twirlApi % "test")
+
+  lazy val sbtPlugin =
+    Project("sbt-twirl", file("sbt-twirl"))
+      .settings(generalSettings: _*)
+      .settings(
+        Keys.sbtPlugin := true
+      )
+      .dependsOn(twirl)
+
+
+  lazy val generalSettings = publishSettings ++ Seq(
+    version               := "0.5.0-SNAPSHOT",
+    homepage              := Some(new URL("https://github.com/spray/sbt-twirl")),
+    organization          := "cc.spray",
+    organizationHomepage  := Some(new URL("http://spray.cc")),
+    startYear             := Some(2012),
+    licenses              := Seq("Apache 2" -> new URL("http://www.apache.org/licenses/LICENSE-2.0.txt")),
+    scalaVersion          := "2.9.1",
+    scalacOptions         := Seq("-unchecked", "-deprecation", "-encoding", "utf8"),
+    description           := "The Play framework Scala template engine, standalone and packaged as an SBT plugin"
+  )
+
+  lazy val publishSettings = Seq[Setting[_]](
+    credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),
+    publishMavenStyle := true,
+    publishTo <<= version { version =>
+      Some {
+        "spray nexus" at {
+          // public uri is repo.spray.cc, we use an SSH tunnel to the nexus here
+          "http://localhost:42424/content/repositories/" + {
+            if (version.trim.endsWith("SNAPSHOT")) "snapshots/" else"releases/"
+          }
+        }
+      }
+    }
+  )
+}
+
+
---
+++ project/Dependencies.scala
@@ -0,0 +1,12 @@
+import sbt._
+
+object Dependencies {
+  val commonsLang = "commons-lang"                  %  "commons-lang"   % "2.6"
+  val scalaIO     = "com.github.scala-incubator.io" %% "scala-io-file"  % "0.2.0"
+
+  object Test {
+    val specs = "org.specs2" %% "specs2" % "1.7.1" % "test"
+  }
+
+  def scalaCompiler(version: String) = "org.scala-lang" % "scala-compiler" % version
+}
