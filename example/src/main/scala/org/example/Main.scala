a3c93ce499126a0df3cd0548e86c6ba438c6ffd2
Mathias <mathias@decodified.com>
2012-02-09 15:26:05 +0100

    Fix example for new (corrected) package naming

--- example/src/main/scala/org/example/Main.scala
+++ example/src/main/scala/org/example/Main.scala
@@ -3,13 +3,13 @@ package org.example
 object Main extends App {
 
   println("A rendered Twirl TXT template:")
-  println(org.txt.example.hello("Alice", 16))
+  println(org.example.txt.hello("Alice", 16))
 
   println("---");
   println("A rendered Twirl HTML template:")
-  println(org.html.example.hello("Bob", 22))
+  println(org.example.html.hello("Bob", 22))
 
   println("---");
   println("A rendered Twirl XML template:")
-  println(org.xml.example.hello("Darth", 42))
+  println(org.example.xml.hello("Darth", 42))
 }
