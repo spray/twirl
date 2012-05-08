807e1c29a5c9ec001ee421e28403521d6de306c8
Mathias <mathias@decodified.com>
2012-05-08 12:40:12 +0200

    Upgrade to SBT 0.11.3

--- project/Build.scala
+++ project/Build.scala
@@ -53,7 +53,7 @@ object Build extends Build {
     organizationHomepage  := Some(new URL("http://spray.cc")),
     startYear             := Some(2012),
     licenses              := Seq("Apache 2" -> new URL("http://www.apache.org/licenses/LICENSE-2.0.txt")),
-    scalaVersion          := "2.9.2",
+    scalaVersion          := "2.9.1",
     scalacOptions         := Seq("-unchecked", "-deprecation", "-encoding", "utf8"),
     description           := "The Play framework Scala template engine, standalone and packaged as an SBT plugin",
     resolvers             += "typesafe repo"   at "http://repo.typesafe.com/typesafe/releases/"
