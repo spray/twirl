274d6d87956c0e356de6e069a5b5dd732756edd2
Johannes Rudolph <johannes_rudolph@gmx.de>
2012-10-24 16:50:07 +0200

    fix #5: depend on the asynchronous version of scala-io, so that we don't depend on akka transitively

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val commonsLang = "commons-lang"                  %  "commons-lang"   % "2.6"
-  val scalaIO     = "com.github.scala-incubator.io" %% "scala-io-file"  % "0.4.1"
+  val scalaIO     = "com.github.scala-incubator.io" %% "scala-io-file"  % "0.4.1-seq"
 
   object Test {
     val specs = "org.specs2" %% "specs2" % "1.12.1" % "test"
